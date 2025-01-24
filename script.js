cfddc1250274328f62543a92f9050de5a7881790
Petr Svoboda <psvoboda1987@gmail.com>
2025-01-24 20:52:58 +0100

    better random

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 function roll(e) {
   const min = 1;
   const max = e.dataset.dice;
-  const result = min + Math.floor(Math.random() * (max - min + 1));
-  document.getElementById("result").innerText = result;
+  document.getElementById("result").innerText = getRandomNumber(min, max);
+}
+
+function getRandomNumber(min, max) {
+  return min + Math.floor(Math.random() * (max - min + 1));
 }
 
 function coinflip(e) {
-  document.getElementById("result").innerText = Math.random() >= 0.5 ? "heads" : "tails";
+  document.getElementById("result").innerText = getRandomNumber(1, 1000) % 2 === 0 ? "heads" : "tails";
 }
